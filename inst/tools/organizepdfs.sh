4ea100d78db634608ed27d7aaad0419aa46b4bc6
Erin Simonds <esimonds@stanford.edu>
2011-03-20 18:55:33 -0700

    Updated organizepdfs.sh to be compatible with newer '.anno.Rsave' filename structure.

--- inst/tools/organizepdfs.sh
+++ inst/tools/organizepdfs.sh
@@ -4,18 +4,22 @@
 #
 # Instructions:  Copy this script to the "pdf" folder in a SPADE output folder, and run it.
 #
-# Version 0.1.0
+# Version 0.2.0
 # Erin Simonds
-# December 14, 2010
+# March 20, 2011
 
 (
 IFS=$'\n'
-filenames=(`ls -1 *.pdf`)
-
+filenames=(`ls -1|grep .*median.*\.pdf$`)
+firstsamplename=(`printf "%s\n" "${filenames[@]}" | head -1 | sed 's/\.fcs.*//g'`)
 
 echo "=== Unique parameters: ==="
-uniqueparams=(`printf "%s\n" "${filenames[@]}" | sed 's/.*\.gml\.median//g' | sed 's/.*\.gml\.fold//g' | sed 's/\_clust//g' | sed 's/.*\.gml\.//g' | sed 's/\.pdf//g' |sort -u`)
+uniqueparams=(`printf "%s\n" "${filenames[@]}" | grep "$firstsamplename" | sed 's/.*\.anno\.Rsave\.medians//g' | sed 's/\.pdf//g' |sort -u`)
 printf "%s\n" "${uniqueparams[@]}"
+
+echo "=== Unique FCS files: ==="
+uniquefcs=(`printf "%s\n" "${filenames[@]}" | sed 's/\.fcs.*//g' | sort -u`)
+printf "%s\n" "${uniquefcs[@]}"
 
 echo "=== Making folder for each parameter: ==="
 mkdir ../pdf_organized_by_parameter
@@ -31,10 +35,6 @@ for i in ${uniqueparams[*]}; do
     cp -v *$i*\.pdf ../pdf_organized_by_parameter/$i
     let COUNT++
 done
-
-echo "=== Unique FCS files: ==="
-uniquefcs=(`printf "%s\n" "${filenames[@]}" | sed 's/\.fcs.*//g' | sort -u`)
-printf "%s\n" "${uniquefcs[@]}"
 
 echo "=== Making folder for each FCS file: ==="
 mkdir ../pdf_organized_by_FCS
