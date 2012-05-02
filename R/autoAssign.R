e68f0b90038eee88ed588e388d1166084a0e30c3
Robert Bruggner <rbruggner@gmail.com>
2012-05-02 14:03:11 -0700

    Debug

--- R/autoAssign.R
+++ R/autoAssign.R
@@ -17,7 +17,7 @@ cleanStr = function(x){
 }
 
 
-evaluateCellTypeRule = function(fcsFileName,transformCols,dataDirectory,ruleDir,ruleFilename) {
+SPADE.evaluateCellTypeRule = function(fcsFileName,transformCols,dataDirectory,ruleDir,ruleFile) {
 #fcsFileName = "Marrow1_01_Unstim1_with_Manual_cell_type_ID.fcs.density.fcs.cluster.fcs"
 #transformCols = c("Ir(190.960)-Dual","Cd(110,111,112,114)","Er(166.932)-Dual","Er(169.935)-Dual","Gd(157.924)-Dual","Gd(159.927)-Dual","In(114.903)-Dual","La(138.906)-Dual","Nd(141.907)-Dual","Nd(143.910)-Dual","Nd(144.912)-Dual","Nd(145.913)-Dual","Nd(147.916)-Dual","Sm(146.914)-Dual")
 
@@ -51,8 +51,8 @@ clusterIds = sort(unique(data[,clusterIndex]))
 
 percents = sapply(transformCols,markerPercents,clusterIds=clusterIds,data=data,medians=medians,clusterIndex=clusterIndex)
 rownames(percents) = clusterIds
-randomPercents = sapply(transformCols,markerPercents,clusterIds=clusterIds,data=dataRandomized,medians=medians,clusterIndex=clusterIndex)
-rownames(percents) = clusterIds
+#randomPercents = sapply(transformCols,markerPercents,clusterIds=clusterIds,data=dataRandomized,medians=medians,clusterIndex=clusterIndex)
+#rownames(percents) = clusterIds
 
 keyMST = read.graph(file=paste(dataDirectory,"mst.gml",sep=""),format="gml")
 layout_table = as.matrix(read.table(file=paste(dataDirectory,"layout.table",sep="")))
@@ -65,17 +65,17 @@ fileMST = read.graph(file=paste(dataDirectory,fcsFileName,".medians.gml",sep="")
 	cat(paste("RULE:",ruleFile,"\n"));
 	populationRules = read.table(paste(ruleDir,ruleFile,sep=""),sep="\t",header=F,col.names=c("parameter","hilo","desc"),comment.char="#")
 	probability=1;
-	randomProbability=1;
+	#randomProbability=1;
 	for (ruleId in 1:nrow(populationRules)){
 		parameter = as.vector(populationRules[ruleId,"parameter"])
 	  if (populationRules[ruleId,"hilo"]=="+"){
 	  	print(paste(parameter,"Hi"))
 	  	probability = probability*percents[,parameter];
-		  randomProbability = randomProbability*randomPercents[,parameter];
+		#randomProbability = randomProbability*randomPercents[,parameter];
 	  } else {
 	  	print(paste(parameter,"Low"))
 	  	probability = probability*(1-percents[,parameter]);
-		  randomProbability = randomProbability*(1-randomPercents[,parameter]);
+		#randomProbability = randomProbability*(1-randomPercents[,parameter]);
 	  }
 	}
 	cat("\n");
@@ -101,3 +101,5 @@ annotation[["autoassign_probability"]]=probabilityMatrix
 
 SPADE.write.graph(SPADE.annotateGraph(fileMST, layout=layout_table, anno=annotation),file=paste(dataDirectory,fcsFileName,".medians.gml",sep=""),format="gml")
 }
+
+
